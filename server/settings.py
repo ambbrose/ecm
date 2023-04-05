9f0a9de502700dbfbddcf82ffdfae86cb21f2565
CodeVeegan <78057753+ambbrose@users.noreply.github.com>
2023-04-05 16:51:04 +0100

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2^ke#ut+$t@-v@t#glcoh7zhs_!ag+vt9@(@o-o8gkmh2#mne9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'ecm-production.up.railway.app']
 
